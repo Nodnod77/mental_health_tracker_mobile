ff624f2bcc64798f8fe95a8b42f09663f38276af
Nodnod77 <donia.sakji@etu.imt-nord-europe.fr>
2024-11-05 22:45:09 +0700

    cleaning

--- mental_health_tracker/lib/menu.dart
+++ mental_health_tracker/lib/menu.dart
@@ -13,11 +13,9 @@ class MyHomePage extends StatelessWidget {
      ];
     @override
   Widget build(BuildContext context) {
-    // Scaffold provides the basic structure of the page with the AppBar and body.
+   
     return Scaffold(
-      // AppBar is the top part of the page that displays the title.
-      appBar: AppBar(
-        // The title of the application "Mental Health Tracker" with white text and bold font.
+      appBar: AppBar(  
         title: const Text(
           'Mental Health Tracker',
           style: TextStyle(
@@ -31,30 +29,22 @@ class MyHomePage extends StatelessWidget {
       // Body of the page with paddings around it.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        // Place the widget vertically in a column.
+        child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // Row to display 3 InfoCard horizontally.
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                InfoCard(title: 'NPM', content: npm),
-                InfoCard(title: 'Name', content: name),
-                InfoCard(title: 'Class', content: className),
-              ],
-            ),
-
-            // Give a vertical space of 16 units.
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(child: InfoCard(title: 'NPM', content: npm)),
+              Expanded(child: InfoCard(title: 'Name', content: name)),
+              Expanded(child: InfoCard(title: 'Class', content: className)),
+            ],
+          ),
             const SizedBox(height: 16.0),
-
-            // Place the following widget in the center of the page.
             Center(
               child: Column(
-                // Place the text and grid item vertically.
-
                 children: [
-                  // Display the welcome message with bold font and size 18.
                   const Padding(
                     padding: EdgeInsets.only(top: 16.0),
                     child: Text(
@@ -65,18 +55,13 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                   ),
-
-                  // Grid to display ItemCard in a 3 column grid.
                   GridView.count(
                     primary: true,
                     padding: const EdgeInsets.all(20),
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                     crossAxisCount: 3,
-                    // To ensure that the grid fits its height.
                     shrinkWrap: true,
-
-                    // Display ItemCard for each item in the items list.
                     children: items.map((ItemHomepage item) {
                       return ItemCard(item);
                     }).toList(),
@@ -86,6 +71,7 @@ class MyHomePage extends StatelessWidget {
             ),
           ],
         ),
+      ),
       ),
     );
   }
